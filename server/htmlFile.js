e247ea5f282c7c25f9e8d4c1c1e9f7d066d63098
Wassim SAIDANE <72514191+wasshisoka44@users.noreply.github.com>
2020-11-12 11:33:49 +0100

    Add files via upload

---
+++ server/htmlFile.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const fs = require('fs').promises;
+const host = 'localhost';
+const port = 8000;
+
+let indexFile;
+const requestListener = function (req, res) {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(indexFile);
+};
+
+const server = http.createServer(requestListener);
+
+fs.readFile(__dirname + "/index.html")
+    .then(contents => {
+        indexFile = contents;
+        server.listen(port, host, () => {
+            console.log(`Server is running on http://${host}:${port}`);
+        });
+    })
+    .catch(err => {
+        console.error(`Could not read index.html file: ${err}`);
+        process.exit(1);
+    });
