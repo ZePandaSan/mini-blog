097ba00acbf68fe711b791f431cfb0bb0e90724e
Wassim SAIDANE <72514191+wasshisoka44@users.noreply.github.com>
2021-01-05 11:25:45 +0100

    Delete delete.php

--- delete.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-include_once 'function/function.php';
-$bdd = bdd();
-
-
-$id = $_GET["id"];
-$requete = $bdd->prepare('DELETE FROM postSujet WHERE id = '.$id);
-$requete->execute();
-header('Location: index.php');
